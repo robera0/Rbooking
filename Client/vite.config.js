f3b11f3d22d5bb76a1677c8ceafbca336541d3e7
robra0 <ararsarobra@gamil.com>
2026-03-23 14:11:23 +0300

    fix-install netlify

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), netlify()],
 
   optimizeDeps: {
     include: ["react", "react-dom"],
