f80ad737fe128eb38c2ab546567aa0ad03f4a7cb
robra0 <ararsarobra@gamil.com>
2026-03-23 21:14:01 +0300

    fix-login form

--- Client/admin/api/admin.api.js
+++ Client/admin/api/admin.api.js
@@ -4,7 +4,7 @@ const API_URL =
   import.meta.env.VITE_API_URL ||
   (import.meta.env.MODE === "development"
     ? "/api/v1"
-    : "https://velvet-1kum.onrender.com/api/v1");
+    : "https://velvet-1kum.onrender.com/api");
 
 export async function getSalons() {
   const response = await axios.get(`${API_URL}/admins/salons?page=2`);
--- Client/user/LoginUser.jsx
+++ Client/user/LoginUser.jsx
@@ -76,68 +76,69 @@ const LoginUser = () => {
       <div className="pl-6 w-full space-y-6">
         {error && <div className="text-red-500 text-sm pl-4">{error}</div>}
         {/*username */}
-        <div className=" relative flex space-x-8 ">
-          <span className="absolute left-4 top-2">
-            <User className="text-white" />
-          </span>
-          <input
-            onChange={(e) => handleEmail(e)}
-            className="placeholder-[#808080] placeholder:text-sm text-white w-[90%] bg-[#323232] pl-12 h-10 rounded-xl outline-none"
-            placeholder="Enter your email"
-            type="text"
-          />
-        </div>
-
-        {/*passwords */}
-        <div className=" relative flex space-x-8 ">
-          <span className="absolute left-4 top-2">
-            <Lock className=" w-5 h-5 text-white" />
-          </span>
-          <input
-            onChange={(e) => handlePassword(e)}
-            className="placeholder-[#808080] placeholder:text-sm text-white w-[90%] bg-[#323232] pl-12 h-10 rounded-xl outline-none"
-            placeholder="Enter your password"
-            type={showPassword ? "text" : "password"}
-          />
-          <span
-            className="absolute right-16 top-3 cursor-pointer"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? (
-              <Eye className=" w-4 h-4 text-white" />
-            ) : (
-              <EyeOff className=" w-4 h-4 text-white" />
-            )}
-          </span>
-        </div>
-        {/*remember me box */}
-        <div className="w-full -mt-2 pl-4 flex justify-between items-center text-[#b3b3b3]">
-          <label className="flex items-center gap-2 cursor-pointer">
-            <input
-              type="checkbox"
-              className="w-4 h-4 accent-[#FF8D28] rounded"
-            />
-            <span className="text-sm font-semibold text-[#b3b3b3]">
-              Remember me
+        <form onSubmit={handleSignin}>
+          <div className=" relative flex space-x-8 ">
+            <span className="absolute left-4 top-2">
+              <User className="text-white" />
             </span>
-          </label>
+            <input
+              onChange={(e) => handleEmail(e)}
+              className="placeholder-[#808080] placeholder:text-sm text-white w-[90%] bg-[#323232] pl-12 h-10 rounded-xl outline-none"
+              placeholder="Enter your email"
+              type="text"
+            />
+          </div>
 
-          <button className="mr-8 text-sm text-[#FF8D28] hover:underline">
-            Forget password?
-          </button>
-        </div>
-      </div>
+          {/*passwords */}
+          <div className=" relative flex space-x-8 ">
+            <span className="absolute left-4 top-2">
+              <Lock className=" w-5 h-5 text-white" />
+            </span>
+            <input
+              onChange={(e) => handlePassword(e)}
+              className="placeholder-[#808080] placeholder:text-sm text-white w-[90%] bg-[#323232] pl-12 h-10 rounded-xl outline-none"
+              placeholder="Enter your password"
+              type={showPassword ? "text" : "password"}
+            />
+            <span
+              className="absolute right-16 top-3 cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <Eye className=" w-4 h-4 text-white" />
+              ) : (
+                <EyeOff className=" w-4 h-4 text-white" />
+              )}
+            </span>
+          </div>
+          {/*remember me box */}
+          <div className="w-full -mt-2 pl-4 flex justify-between items-center text-[#b3b3b3]">
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="checkbox"
+                className="w-4 h-4 accent-[#FF8D28] rounded"
+              />
+              <span className="text-sm font-semibold text-[#b3b3b3]">
+                Remember me
+              </span>
+            </label>
 
-      {/*Login button */}
-      <div className="w-full flex justify-center">
-        <button
-          onClick={(e) => {
-            handleSignin(e);
-          }}
-          className=" flex  items-center  justify-center bg-[#FF7800] text-white w-[70%] h-10 text-md font-semibold rounded-xl cursor-pointer "
-        >
-          Login
-        </button>
+            <button className="mr-8 text-sm text-[#FF8D28] hover:underline">
+              Forget password?
+            </button>
+          </div>
+          {/*Login button */}
+          <div className="w-full flex justify-center">
+            <button
+              onClick={(e) => {
+                handleSignin(e);
+              }}
+              className=" flex  items-center  justify-center bg-[#FF7800] text-white w-[70%] h-10 text-md font-semibold rounded-xl cursor-pointer "
+            >
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
